dbf9d98f2b2ba7eeebdb3a2b371d453060c743ff
GiovanniCorvi <g.corvi@student.tudelft.nl>
2022-03-21 16:35:04 +0100

    score

--- main.py
+++ main.py
@@ -65,7 +65,11 @@ imagePistolRect.center = (xc+250, yc+30)
 font = pygame.font.Font('freesansbold.ttf', 15) # printing text font and font size
 textHits = font.render('Hits: ', True, (0, 0, 0), (255, 255, 255)) # printing text object
 textHitsRect = textHits.get_rect()
-textHitsRect.topleft = (10, 30) 
+textHitsRect.topleft = (10, 30)
+
+textCurrScore = font.render('SCORE:     ', True, (0, 0, 0), (255, 255, 255)) # printing text object
+textCurrScoreRect = textCurrScore.get_rect()
+textCurrScoreRect.center = (xc, 15)  
 
 textTime = font.render('Time: ', True, (0, 0, 0), (255, 255, 255)) # printing text object
 textTimeRect = textTime.get_rect()
@@ -271,16 +275,19 @@ while run:
                     setTimer = 1
                     gun = "sniper"
                     startscreen = False
+                    score = 0
                     minDistanceList = []  # create empty minDistanceList
                 if event.key == ord('r'): # select rifle
                     setTimer = 1
                     gun = "rifle"
                     startscreen = False
+                    score = 0
                     minDistanceList = []  # create empty minDistanceList
                 if event.key == ord('p'): # select pistol
                     setTimer = 1
                     gun = "pistol"
                     startscreen = False
+                    score = 0
                     minDistanceList = []  # create empty minDistanceList
         
         # real-time plotting
@@ -345,7 +352,7 @@ while run:
                 run = False
             if event.key == pygame.K_SPACE: # shoot with 'space' button
                 bulletCount += 1
-                
+                hit_smth= False
                 shooting = True
                 recoil_duration = 12
                 
@@ -356,6 +363,8 @@ while run:
                     if np.sqrt((xh[0]-int(target.pos[0]))**2 + (xh[1] -int(target.pos[1]))**2)<radius:
                         target.hit()
                         killCount += 1
+                        score += 50
+                        hit_smth= True
                         
                 minDistance = min(distanceList)
                 minDistanceList.append(minDistance)
@@ -363,10 +372,12 @@ while run:
                 for civ in civilian_list:
                     if np.sqrt((xh[0]-int(civ.pos[0]))**2 + (xh[1] -int(civ.pos[1]))**2)<radius:
                         civ.hit()
-                        
+                        score -= 100
+                        hit_smth= True
                 
-                #print(minDistanceList)
-                #print(sum(minDistanceList)/bulletCount)
+                if not hit_smth:
+                    score -= 20
+                
                     
     # start timer
     if setTimer == 1:
@@ -442,12 +453,16 @@ while run:
     window.blit(imageBgd1, (0, 0))
     
     # plot time
-    textTime = font.render('Time: '+ str(countdown), True, (0, 0, 0), (255, 255, 255))
+    textTime = font.render('Time: '+ str(countdown), True, (0, 0, 0))
     window.blit(textTime, textTimeRect)
     
     # plot kill count
-    textHits = font.render('HITS: '+ str(killCount), True, (255, 0, 0), (255, 255, 255))
+    textHits = font.render('HITS: '+ str(killCount), True, (255, 0, 0))
     window.blit(textHits, textHitsRect)
+    
+    #plot current score
+    textCurrScore = font.render('Score: '+ str(score), True, (0, 0, 0))
+    window.blit(textCurrScore, textCurrScoreRect)
     
     # plot target
     #pygame.draw.circle(window, (0, 255, 0), (x_rand, y_rand), radius)
@@ -543,6 +558,8 @@ while run:
             ResultSME = 100000000
         else:
             ResultSME =  round(sum(minDistanceList)/bulletCount, 2)
+            
+        score = score + int(5*1e4/ResultSME)
     '''*********** ENDSCRREEN ***********'''
     while endscreen:  
         for event in pygame.event.get(): # interrupt function
@@ -573,6 +590,9 @@ while run:
         window.fill((255,255,255)) # clear window
     
         # plot text to screen
+        textScore = fontTitle.render('Score:  ' + str(score), True, (0, 0, 0), (255, 255, 255))
+        textScoreRect = textScore.get_rect()
+        textScoreRect.center = (xc, yc-200)
         window.blit(textScore, textScoreRect)
         
         textKPM = fontTable.render('Kills per minute: ' + str(kills_per_minute), True, (0, 0, 0), (255, 255, 255)) # printing text object
